fa33fdbc9d6f5b5b144e6f35e283cf5817a43d79
Jordan Ramirez <jordan.s.ramirez@gmail.com>
2022-01-30 16:43:41 -0500

    more wifi stuff

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,9 @@
 #include "DNSServer.h"
 
 // Server
-// WebServer server(80);
 AsyncWebServer server(80); // This creates a web server, required in order to host a page for connected devices
 DNSServer dnsServer;       // This creates a DNS server, required for the captive portal
+WiFiUDP udp;
 
 struct settings
 {
@@ -21,23 +21,6 @@ struct settings
 
 // Got info?
 bool gotInfo = false;
-
-// void handlePortal() {
-//   if (server.method() == HTTP_POST) {
-//     // Saving WiFi Data to the flash
-//     strncpy(user_wifi.ssid,     server.arg("ssid").c_str(),     sizeof(user_wifi.ssid) );
-//     strncpy(user_wifi.password, server.arg("password").c_str(), sizeof(user_wifi.password) );
-//     user_wifi.ssid[server.arg("ssid").length()] = user_wifi.password[server.arg("password").length()] = '\0';
-//     EEPROM.put(0, user_wifi);
-//     EEPROM.commit();
-
-//     server.send(200,   "text/html",  "<!doctype html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><title>Wifi Setup</title><style>*,::after,::before{box-sizing:border-box;}body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';font-size:1rem;font-weight:400;line-height:1.5;color:#212529;background-color:#f5f5f5;}.form-control{display:block;width:100%;height:calc(1.5em + .75rem + 2px);border:1px solid #ced4da;}button{border:1px solid transparent;color:#fff;background-color:#007bff;border-color:#007bff;padding:.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:.3rem;width:100%}.form-signin{width:100%;max-width:400px;padding:15px;margin:auto;}h1,p{text-align: center}</style> </head> <body><main class='form-signin'> <h1>Wifi Setup</h1> <br/> <p>Your settings have been saved successfully!<br />Please restart the device.</p></main></body></html>" );
-//     gotInfo = true;
-//   } else {
-
-//     server.send(200,   "text/html", "<!doctype html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><title>Wifi Setup</title> <style>*,::after,::before{box-sizing:border-box;}body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';font-size:1rem;font-weight:400;line-height:1.5;color:#212529;background-color:#f5f5f5;}.form-control{display:block;width:100%;height:calc(1.5em + .75rem + 2px);border:1px solid #ced4da;}button{cursor: pointer;border:1px solid transparent;color:#fff;background-color:#007bff;border-color:#007bff;padding:.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:.3rem;width:100%}.form-signin{width:100%;max-width:400px;padding:15px;margin:auto;}h1{text-align: center}</style> </head> <body><main class='form-signin'> <form action='/' method='post'> <h1 class=''>MicroController Greens Wifi Setup</h1><br/><div class='form-floating'><label>SSID</label><input type='text' class='form-control' name='ssid'> </div><div class='form-floating'><br/><label>Password</label><input type='password' class='form-control' name='password'></div><br/><br/><button type='submit'>Save</button><p style='text-align: right'></p></form></main> </body></html>" );
-//   }
-// }
 
 void connectToWifi()
 {
@@ -86,51 +69,63 @@ void onGet(AsyncWebServerRequest *request)
   String ssid;
   String password;
 
+  String params[] = {request->getParam(0)->name(), request->getParam(1)->name()};
+
+  // CHECK Which Type
+  // if(request->hasParam("ssid")) {
+  //   Serial.println("No True");
+  // }
+  // if(request->hasParam("ssid", true)) {
+  //   Serial.println("One True");
+  // }
+  // if(request->hasParam("ssid", true, true)) {
+  //   Serial.println("Two True");
+  // }
+
   // Checking for inputs
-  if (request->hasParam("ssid"))
-  {
-    ssid = request->getParam("ssid")->value();
+  if (request->hasParam("ssid", true)) {
+    ssid = request->getParam("ssid", true)->value();
     Serial.println("SSID: " + ssid);
   }
-  if (request->hasParam("password"))
-  {
-    password = request->getParam("password")->value();
+  if (request->hasParam("password"), true) {
+    password = request->getParam("password", true)->value();
     Serial.println("Password: " + password);
   }
 
-  // Check for length less than 30
-  if ((password.length() <= 30) || (ssid.length() <= 30))
-  {
-    // Update webpage
-    request->send(200, "text/html", "WiFi SSID: " + ssid + ", and Password: " + password + " Saved! Trying connection...");
+  // // Check for length less than 30
+  // if ((password.length() <= 30) || (ssid.length() <= 30))
+  // {
+  //   // Update webpage
+  //   request->send(200, "text/html", "WiFi SSID: " + ssid + ", and Password: " + password + " Saved! Trying connection...");
 
-    // Save to FLASH
-    strncpy(user_wifi.ssid, ssid.c_str(), sizeof(user_wifi.ssid));
-    strncpy(user_wifi.password, password.c_str(), sizeof(user_wifi.password));
-    user_wifi.ssid[ssid.length()] = user_wifi.password[password.length()] = '\0';
+  //   // Save to FLASH
+  //   strncpy(user_wifi.ssid, ssid.c_str(), sizeof(user_wifi.ssid));
+  //   strncpy(user_wifi.password, password.c_str(), sizeof(user_wifi.password));
+  //   user_wifi.ssid[ssid.length()] = user_wifi.password[password.length()] = '\0';
 
-    // Connect to Wifi
-    connectToWifi();
-    request->send(200, "text/html", "MicroController Greens Connected!");
+  //   // Connect to Wifi
+  //   connectToWifi();
+  //   request->send(200, "text/html", "MicroController Greens Connected!");
 
-    if (WL_CONNECTED) {
-      // Save to FLASH
-      EEPROM.put(0, user_wifi);
-      EEPROM.commit();
+  //   if (WL_CONNECTED) {
+  //     // Save to FLASH
+  //     EEPROM.put(0, user_wifi);
+  //     EEPROM.commit();
 
-      Serial.println("SSID and Password saved to FLASH");
-      Serial.println("SSID: " + ssid);
-      Serial.println("Password: " + password);
+  //     Serial.println("SSID and Password saved to FLASH");
+  //     Serial.println("SSID: " + ssid);
+  //     Serial.println("Password: " + password);
 
-      // Shut Down DNS Server
-      dnsServer.stop();
-    }
-  }
-  else
-  {
-    Serial.println("SSID or Password Too long");
-    request->send(200, "text/html", "<!doctype html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><p>Wifi SSID or Password too long, please try again</p><title>MicroController Greens Wifi Setup</title> <style>*,::after,::before{box-sizing:border-box;}body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';font-size:1rem;font-weight:400;line-height:1.5;color:#212529;background-color:#f5f5f5;}.form-control{display:block;width:100%;height:calc(1.5em + .75rem + 2px);border:1px solid #ced4da;}button{cursor: pointer;border:1px solid transparent;color:#fff;background-color:#007bff;border-color:#007bff;padding:.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:.3rem;width:100%}.form-signin{width:100%;max-width:400px;padding:15px;margin:auto;}h1{text-align: center}</style> </head> <body><main class='form-signin'> <form action='/get' method='post'> <h1 class=''>MicroController Greens Wifi Setup</h1><br/><div class='form-floating'><label>SSID</label><input type='text' class='form-control' name='ssid'> </div><div class='form-floating'><br/><label>Password</label><input type='password' class='form-control' name='password'></div><br/><br/><button type='submit'>Save</button><p style='text-align: right'></p></form></main> </body></html>");
-  }
+  //     // Shut Down DNS Server
+  //     dnsServer.stop();
+  //   }
+  // }
+  // else
+  // {
+  //   Serial.println("SSID or Password Too long");
+  //   request->send(200, "text/html", "<!doctype html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><p>Wifi SSID or Password too long, please try again</p><title>MicroController Greens Wifi Setup</title> <style>*,::after,::before{box-sizing:border-box;}body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';font-size:1rem;font-weight:400;line-height:1.5;color:#212529;background-color:#f5f5f5;}.form-control{display:block;width:100%;height:calc(1.5em + .75rem + 2px);border:1px solid #ced4da;}button{cursor: pointer;border:1px solid transparent;color:#fff;background-color:#007bff;border-color:#007bff;padding:.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:.3rem;width:100%}.form-signin{width:100%;max-width:400px;padding:15px;margin:auto;}h1{text-align: center}</style> </head> <body><main class='form-signin'> <form action='/get' method='post'> <h1 class=''>MicroController Greens Wifi Setup</h1><br/><div class='form-floating'><label>SSID</label><input type='text' class='form-control' name='ssid'> </div><div class='form-floating'><br/><label>Password</label><input type='password' class='form-control' name='password'></div><br/><br/><button type='submit'>Save</button><p style='text-align: right'></p></form></main> </body></html>");
+  // }
+
 }
 
 void webServerSetup(){ 
@@ -148,57 +143,6 @@ void webServerSetup(){
     Serial.println(request->url());
     webRequest(request);
   });
-
-  // Reading the Inputs
-  // server.on("/get", HTTP_GET, [](AsyncWebServerRequest *request) {
-  //   String ssid;
-  //   String password;
-    
-  //   // Checking for inputs
-  //   if(request->hasParam("ssid")) {
-  //     ssid = request->getParam("ssid")->value();
-  //     Serial.println("SSID: " + ssid);
-  //   }
-  //   if(request->hasParam("password")) {
-  //     password = request->getParam("password")->value();
-  //     Serial.println("Password: " + password);
-  //   }
-
-  //   // Check for length less than 30
-  //   if((password.length() <= 30) || (ssid.length() <= 30)) {
-  //     // Update webpage
-  //     request->send(200, "text/html", "WiFi SSID: " + ssid + ", and Password: " + password + " Saved! Trying connection...");
-
-  //     // Save to FLASH
-  //     strncpy(user_wifi.ssid, ssid.c_str(), sizeof(user_wifi.ssid) );
-  //     strncpy(user_wifi.password, password.c_str(), sizeof(user_wifi.password) );
-  //     user_wifi.ssid[ssid.length()] = user_wifi.password[password.length()] = '\0';
-
-  //     // Connect to Wifi
-  //     connectToWifi();
-  //     request->send(200, "text/html", "MicroController Greens Connected!");
-
-  //     if(!WL_CONNECTED) {
-  //       // Shut Down DNS Server
-  //       dnsServer.stop();
-
-  //     } 
-  //     else {
-  //       // Save to FLASH
-  //       EEPROM.put(0, user_wifi);
-  //       EEPROM.commit();  
-
-  //       Serial.println("SSID and Password saved to FLASH"); 
-  //       Serial.println("SSID: " + ssid);
-  //       Serial.println("Password: " + password);
-  //     }
-  //   }
-  //   else {
-  //     Serial.println("SSID or Password Too long");
-  //     request->send(200, "text/html", "<!doctype html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><p>Wifi SSID or Password too long, please try again</p><title>MicroController Greens Wifi Setup</title> <style>*,::after,::before{box-sizing:border-box;}body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';font-size:1rem;font-weight:400;line-height:1.5;color:#212529;background-color:#f5f5f5;}.form-control{display:block;width:100%;height:calc(1.5em + .75rem + 2px);border:1px solid #ced4da;}button{cursor: pointer;border:1px solid transparent;color:#fff;background-color:#007bff;border-color:#007bff;padding:.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:.3rem;width:100%}.form-signin{width:100%;max-width:400px;padding:15px;margin:auto;}h1{text-align: center}</style> </head> <body><main class='form-signin'> <form action='/get' method='post'> <h1 class=''>MicroController Greens Wifi Setup</h1><br/><div class='form-floating'><label>SSID</label><input type='text' class='form-control' name='ssid'> </div><div class='form-floating'><br/><label>Password</label><input type='password' class='form-control' name='password'></div><br/><br/><button type='submit'>Save</button><p style='text-align: right'></p></form></main> </body></html>" );
-  //   }
-  // 
-  // });
 
   //onNotFound acts as a catch-all for any request not defined above
   server.onNotFound([](AsyncWebServerRequest *request){
