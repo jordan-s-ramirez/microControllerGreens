a31e6731f794cb30a827e550ebe3b9e7d71f0834
Jordan Ramirez <jordan.s.ramirez@gmail.com>
2022-01-30 18:59:44 -0500

    wifi stuff

--- src/main.cpp
+++ src/main.cpp
@@ -27,33 +27,34 @@ void connectToWifi()
   // Make WiFi as a Station
   WiFi.mode(WIFI_STA);
   // Check to see if we got info
-  if (gotInfo)
+  if (user_wifi.ssid[0] != 0)
   {
-    // Connect to WiFi
+    Serial.println("Trying Connection with:");
+    Serial.println(user_wifi.ssid);
+    Serial.println(user_wifi.password);
+
     WiFi.begin(user_wifi.ssid, user_wifi.password);
 
     // Try WiFi
     byte tries = 0;
-    while (WiFi.status() != WL_CONNECTED)
+    // Connect to WiFi
+    while ((WiFi.status() != WL_CONNECTED ) && tries < 5)
     {
-      delay(500);
+      vTaskDelay(500 / portTICK_PERIOD_MS);
       Serial.println("Trying to Connect... ");
-      if (tries++ > 30)
-      {
-        break;
-      }
+      tries++;
     }
   }
 
   // Check WiFi
   if (WiFi.status() != WL_CONNECTED)
   {
-    Serial.println("Failed!");
+    Serial.println("Failed to Connect to WiFi!");
     gotInfo = false;
   }
   else
   {
-    Serial.print("Connected!\n");
+    Serial.print("Connected to WiFi!\n");
     Serial.println(WiFi.localIP());
     gotInfo = true;
   }
@@ -64,12 +65,23 @@ void webRequest(AsyncWebServerRequest *request)
   request->send(200, "text/html", "<!doctype html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><title>MicroController Greens Wifi Setup</title> <style>*,::after,::before{box-sizing:border-box;}body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';font-size:1rem;font-weight:400;line-height:1.5;color:#212529;background-color:#f5f5f5;}.form-control{display:block;width:100%;height:calc(1.5em + .75rem + 2px);border:1px solid #ced4da;}button{cursor: pointer;border:1px solid transparent;color:#fff;background-color:#007bff;border-color:#007bff;padding:.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:.3rem;width:100%}.form-signin{width:100%;max-width:400px;padding:15px;margin:auto;}h1{text-align: center}</style> </head> <body><main class='form-signin'> <form action='/get' method='post'> <h1 class=''>MicroController Greens Wifi Setup</h1><br/><div class='form-floating'><label>SSID</label><input type='text' class='form-control' name='ssid'> </div><div class='form-floating'><br/><label>Password</label><input type='password' class='form-control' name='password'></div><br/><br/><button type='submit'>Save</button><p style='text-align: right'></p></form></main> </body></html>");
 }
 
+void createSAP() {
+  // Enabling WiFi as soft access point
+  WiFi.mode(WIFI_MODE_AP);
+
+  Serial.println("Setting Soft Access Point: Setup");
+  Serial.println(WiFi.softAP("MicroController Greens", NULL) ? "Ready" : "Failed!");
+
+  Serial.print("SAP IP Address: ");
+  Serial.println(WiFi.softAPIP());
+
+}
+
+
 void onGet(AsyncWebServerRequest *request)
 {
   String ssid;
   String password;
-
-  String params[] = {request->getParam(0)->name(), request->getParam(1)->name()};
 
   // CHECK Which Type
   // if(request->hasParam("ssid")) {
@@ -92,41 +104,61 @@ void onGet(AsyncWebServerRequest *request)
     Serial.println("Password: " + password);
   }
 
-  // // Check for length less than 30
-  // if ((password.length() <= 30) || (ssid.length() <= 30))
-  // {
-  //   // Update webpage
-  //   request->send(200, "text/html", "WiFi SSID: " + ssid + ", and Password: " + password + " Saved! Trying connection...");
+  // Check for length less than 30
+  if ((password.length() <= 30) || (ssid.length() <= 30))
+  {
+    // Update webpage
+    request->send(200, "text/html", "<h1>WiFi SSID: " + ssid + ", and Password: " + password + " Saved! Trying connection...</h1>");
 
-  //   // Save to FLASH
-  //   strncpy(user_wifi.ssid, ssid.c_str(), sizeof(user_wifi.ssid));
-  //   strncpy(user_wifi.password, password.c_str(), sizeof(user_wifi.password));
-  //   user_wifi.ssid[ssid.length()] = user_wifi.password[password.length()] = '\0';
+    // Save to FLASH
+    strncpy(user_wifi.ssid, ssid.c_str(), sizeof(user_wifi.ssid));
+    strncpy(user_wifi.password, password.c_str(), sizeof(user_wifi.password));
+    user_wifi.ssid[ssid.length()] = user_wifi.password[password.length()] = '\0';
 
-  //   // Connect to Wifi
-  //   connectToWifi();
-  //   request->send(200, "text/html", "MicroController Greens Connected!");
+    // Connect to Wifi
+    connectToWifi();
 
-  //   if (WL_CONNECTED) {
-  //     // Save to FLASH
-  //     EEPROM.put(0, user_wifi);
-  //     EEPROM.commit();
+    if (WiFi.status() == WL_CONNECTED) {
+      //request->send(200, "text/html", "<h1>MicroController Greens has Connected to WiFi</h1>");
 
-  //     Serial.println("SSID and Password saved to FLASH");
-  //     Serial.println("SSID: " + ssid);
-  //     Serial.println("Password: " + password);
+      // Save to FLASH
+      EEPROM.put(0, user_wifi);
+      EEPROM.commit();
 
-  //     // Shut Down DNS Server
-  //     dnsServer.stop();
-  //   }
-  // }
-  // else
-  // {
-  //   Serial.println("SSID or Password Too long");
-  //   request->send(200, "text/html", "<!doctype html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><p>Wifi SSID or Password too long, please try again</p><title>MicroController Greens Wifi Setup</title> <style>*,::after,::before{box-sizing:border-box;}body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';font-size:1rem;font-weight:400;line-height:1.5;color:#212529;background-color:#f5f5f5;}.form-control{display:block;width:100%;height:calc(1.5em + .75rem + 2px);border:1px solid #ced4da;}button{cursor: pointer;border:1px solid transparent;color:#fff;background-color:#007bff;border-color:#007bff;padding:.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:.3rem;width:100%}.form-signin{width:100%;max-width:400px;padding:15px;margin:auto;}h1{text-align: center}</style> </head> <body><main class='form-signin'> <form action='/get' method='post'> <h1 class=''>MicroController Greens Wifi Setup</h1><br/><div class='form-floating'><label>SSID</label><input type='text' class='form-control' name='ssid'> </div><div class='form-floating'><br/><label>Password</label><input type='password' class='form-control' name='password'></div><br/><br/><button type='submit'>Save</button><p style='text-align: right'></p></form></main> </body></html>");
-  // }
+      Serial.println("SSID and Password saved to FLASH");
+      Serial.println("SSID: " + ssid);
+      Serial.println("Password: " + password);
+
+      // Shut Down DNS Server
+      Serial.println("Server SHUTDOWN");
+      server.end();
+      dnsServer.stop();
+      WiFi.softAPdisconnect(); 
+    }
+    else {
+      // Update User
+      // request->send(200, "text/html", "<!doctype html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><h1>Incorrect WiFi SSID or password, please try again.</h1><title>MicroController Greens Wifi Setup</title> <style>*,::after,::before{box-sizing:border-box;}body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';font-size:1rem;font-weight:400;line-height:1.5;color:#212529;background-color:#f5f5f5;}.form-control{display:block;width:100%;height:calc(1.5em + .75rem + 2px);border:1px solid #ced4da;}button{cursor: pointer;border:1px solid transparent;color:#fff;background-color:#007bff;border-color:#007bff;padding:.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:.3rem;width:100%}.form-signin{width:100%;max-width:400px;padding:15px;margin:auto;}h1{text-align: center}</style> </head> <body><main class='form-signin'> <form action='/get' method='post'> <h1 class=''>MicroController Greens Wifi Setup</h1><br/><div class='form-floating'><label>SSID</label><input type='text' class='form-control' name='ssid'> </div><div class='form-floating'><br/><label>Password</label><input type='password' class='form-control' name='password'></div><br/><br/><button type='submit'>Save</button><p style='text-align: right'></p></form></main> </body></html>");
+      
+      // Clean User Wifi and Password
+      Serial.print("Cleaning");
+      for(int i = 0; i < 30; i++) {
+        Serial.print(".");
+        user_wifi.password[i] = 0;
+        user_wifi.ssid[i] = 0;
+      }
+      // createSAP();
+      //createWebServer();
+    }
+  }
+  else
+  {
+    Serial.println("SSID or Password Too long");
+    request->send(200, "text/html", "<!doctype html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><h1>Wifi SSID or Password too long, please try again.</h1><title>MicroController Greens Wifi Setup</title> <style>*,::after,::before{box-sizing:border-box;}body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';font-size:1rem;font-weight:400;line-height:1.5;color:#212529;background-color:#f5f5f5;}.form-control{display:block;width:100%;height:calc(1.5em + .75rem + 2px);border:1px solid #ced4da;}button{cursor: pointer;border:1px solid transparent;color:#fff;background-color:#007bff;border-color:#007bff;padding:.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:.3rem;width:100%}.form-signin{width:100%;max-width:400px;padding:15px;margin:auto;}h1{text-align: center}</style> </head> <body><main class='form-signin'> <form action='/get' method='post'> <h1 class=''>MicroController Greens Wifi Setup</h1><br/><div class='form-floating'><label>SSID</label><input type='text' class='form-control' name='ssid'> </div><div class='form-floating'><br/><label>Password</label><input type='password' class='form-control' name='password'></div><br/><br/><button type='submit'>Save</button><p style='text-align: right'></p></form></main> </body></html>");
+  
+  }
 
 }
+
 
 void webServerSetup(){ 
   // //This is a super simple page that will be served up any time the root location is requested.  Get here intentionally by typing in the IP address.
@@ -161,18 +193,6 @@ void webServerSetup(){
   Serial.println("Web server started");
 }
 
-void createSAP() {
-  // Enabling WiFi as soft access point
-  WiFi.mode(WIFI_MODE_AP);
-
-  Serial.println("Setting Soft Access Point: Setup");
-  Serial.println(WiFi.softAP("MicroController Greens", NULL) ? "Ready" : "Failed!");
-
-  Serial.print("SAP IP Address: ");
-  Serial.println(WiFi.softAPIP());
-
-}
-
 void createWebServer() {
   // DNS Stuff
   dnsServer.start(53, "*", WiFi.softAPIP());  //This starts the DNS server.  The "*" sends any request for port 53 straight to the IP address of the device
@@ -183,12 +203,9 @@ void createWebServer() {
 void setup() {
   Serial.begin(9600);
   // Allocate Storage For WiFi
-  // THIS IS A WILL DELETE THE SAVED INPUT DELETE THE LINE BEWLOW
-  for (int i = 0 ; i < EEPROM.length() ; i++) {
-    EEPROM.write(i, 0);
-  }
 
   EEPROM.begin(sizeof(struct settings) );
+  EEPROM.put(0, user_wifi); // DELETES PREVIOSU SAVED WIFI SETTINGS
   EEPROM.get(0, user_wifi);
   
   // Try WiFi Connection, will create SAP if fails
@@ -204,7 +221,6 @@ void setup() {
 
 void loop() {
   if(!gotInfo) {
-    //Serial.println("Getting INFO");
     //server.handleClient();
     dnsServer.processNextRequest(); 
   }
