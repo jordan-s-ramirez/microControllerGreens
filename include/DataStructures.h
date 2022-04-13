bc709935978e0a9f053a19ec0d9b9053963db214
Zach Cawood <zcawood@purdue.edu>
2022-04-13 16:08:50 -0400

    removed MEDIUM from enums to avoid redefined error

--- include/DataStructures.h
+++ include/DataStructures.h
@@ -1,8 +1,9 @@
 #pragma once
+#include <Arduino.h>
 // Enums
-enum LightLevel {DARK, MEDIUM_DARK, MEDIUM, MEDIUM_BRIGHT, BRIGHT};
+enum LightLevel {DARK, MEDIUM_DARK, MEDIUM_BRIGHT, BRIGHT};
 typedef enum LightLevel LightLevel;
-enum WaterLevel {DRY, MEDIUM_DRY, MEDIUM, MEDIUM_WET, WET};
+enum WaterLevel {DRY, MEDIUM_DRY, MEDIUM_WET, WET};
 typedef enum WaterLevel WaterLevel;
 
 // Preferences - returned from wifi loop, sent to hardware loop, determines actions of lights and pump
