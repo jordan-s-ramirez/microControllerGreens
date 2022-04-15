f1960c411f8090221909acee81d8678cc2e384a5
Zach Cawood <zcawood@purdue.edu>
2022-04-15 17:40:58 -0400

    added pump logic

--- src/Hardware.cpp
+++ src/Hardware.cpp
@@ -19,37 +19,61 @@
 #define SOILMOISTURE_PIN 15
   // Pump
 #define PUMP_PIN 27
+    // seconds pump is turned on when soil moisture is low
+#define PUMP_ON_TIME 5
+    // seconds after pump is turned on that the pump can be turned on again
+    // (gives time for soil to absorb water to avoid overwatering)
+#define PUMP_COOLDOWN 30
+    // calibration values
+#define DRY_SOIL_MEASUREMENT 2600
+#define WET_SOIL_MEASUREMENT 1400
+
+
+// Variables
+static unsigned long lastPumpOnTime = millis();
+static unsigned long lastPumpCooldownTime = millis();
 
 // Code
 // Sets up PWM (for lights), Light sensor, Break beam sensor, Soil moisture sensor, and Pump
 void hardwareSetup() {
-    ////////////////////////////////////////////////
-    // Set up Light Sensor
-    setupLightSensor(LIGHT_CEN, LIGHT_SCL, LIGHT_SDA);
-    ////////////////////////////////////////
-    // Set up soil moisture sensor
-    pinMode(SOILMOISTURE_PIN, INPUT);
-    ////////////////////////////////////////////
-    // Set up break beam sensor
-    pinMode(BREAKBEAM_PIN, INPUT);
-    ////////////////////////////////////////
-    // Set up PWM
-    setupPWM(PWM_CHANNEL, PWM_FREQUENCY, PWM_RESOLUTION, PWM_PIN);
-    ////////////////////////////////////////////
-    // Set up pump
-    pinMode(PUMP_PIN, OUTPUT);
+  ////////////////////////////////////////////////
+  // Set up Light Sensor
+  setupLightSensor(LIGHT_CEN, LIGHT_SCL, LIGHT_SDA);
+  ////////////////////////////////////////
+  // Set up soil moisture sensor
+  pinMode(SOILMOISTURE_PIN, INPUT);
+  ////////////////////////////////////////////
+  // Set up break beam sensor
+  pinMode(BREAKBEAM_PIN, INPUT);
+  ////////////////////////////////////////
+  // Set up PWM
+  setupPWM(PWM_CHANNEL, PWM_FREQUENCY, PWM_RESOLUTION, PWM_PIN);
+  ////////////////////////////////////////////
+  // Set up pump
+  pinMode(PUMP_PIN, OUTPUT);
 }
 
 // Takes measurements from sensors, changes lights or runs pump if necessary,
 // and returns the measurements
 Measurements hardwareLoop(Preferences preferences) {
-    Measurements measurements = {0};
-    // Take measurements
-    measurements.light = getLightValue();
-    measurements.water = analogRead(SOILMOISTURE_PIN);
-    measurements.breakBeam = (bool) digitalRead(BREAKBEAM_PIN);
-    // Do stuff based on preferences and measurements
-    // TODO (PWM(lights) and Pump)
-
-    return measurements;
+  Measurements measurements = {0};
+  // Take measurements
+  measurements.light = getLightValue();
+  measurements.water = analogRead(SOILMOISTURE_PIN);
+  measurements.breakBeam = (bool) digitalRead(BREAKBEAM_PIN);
+  // Do stuff based on preferences and measurements
+  // Pump
+  unsigned long now = millis();
+  // if past the cooldown time and soil moisture is above(dryer) than preference indicates, turn pump on and reset times
+  if ((now - lastPumpCooldownTime)/1000 > PUMP_COOLDOWN && measurements.water > (DRY_SOIL_MEASUREMENT - ((int)preferences.waterLevel * ((DRY_SOIL_MEASUREMENT - WET_SOIL_MEASUREMENT)/5)))) {
+    lastPumpOnTime = now;
+    lastPumpCooldownTime = now;
+    digitalWrite(PUMP_PIN, HIGH);
+  }
+  // if past pump on time, turn it off
+  if ((now - lastPumpOnTime)/1000 > PUMP_ON_TIME) {
+    digitalWrite(PUMP_PIN, LOW);
+  }
+  // TODO (Lights w/ PWM) -> need calibration vals
+  return measurements;
 }
